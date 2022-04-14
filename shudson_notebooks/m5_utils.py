04cc733021ebd1edee1d21eb40c2f8784deb478a
Stephen Hudson <stephen.donald.hudson@gmail.com>
2022-04-13 21:34:47 -0400

    Tested changes to model features

--- shudson_notebooks/m5_utils.py
+++ shudson_notebooks/m5_utils.py
@@ -34,3 +34,103 @@ def m5_evaluation(forecast, truth=None, method='rmse'):
         raise ValueError(f'{method.upper()} is not a supported evaluation metric')
         
     return forecast_score
+
+
+def introduce_nulls(df):
+    idx = pd.date_range(df.date.dt.date.min(), df.date.dt.date.max())
+    df = df.set_index('date')
+    df = df.reindex(idx)
+    df.reset_index(inplace=True)
+    df.rename(columns={'index':'date'},inplace=True)
+    return df
+
+def plot_metric(df,state,store,metric):
+    store_sales = df[(df['state_id']==state)&(df['store_id']==store)&(df['date']<='2016-05-22')]
+    food_sales = store_sales[store_sales['cat_id']=='FOODS']
+    store_sales = store_sales.groupby(['date','snap_'+state],as_index=False)['sold','revenue'].sum()
+    snap_sales = store_sales[store_sales['snap_'+state]==1]
+    non_snap_sales = store_sales[store_sales['snap_'+state]==0]
+    food_sales = food_sales.groupby(['date','snap_'+state],as_index=False)['sold','revenue'].sum()
+    snap_foods = food_sales[food_sales['snap_'+state]==1]
+    non_snap_foods = food_sales[food_sales['snap_'+state]==0]
+    non_snap_sales = introduce_nulls(non_snap_sales)
+    snap_sales = introduce_nulls(snap_sales)
+    non_snap_foods = introduce_nulls(non_snap_foods)
+    snap_foods = introduce_nulls(snap_foods)
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=non_snap_sales['date'],y=non_snap_sales[metric],
+                           name='Total '+metric+'(Non-SNAP)'))
+    fig.add_trace(go.Scatter(x=snap_sales['date'],y=snap_sales[metric],
+                           name='Total '+metric+'(SNAP)'))
+    fig.add_trace(go.Scatter(x=non_snap_foods['date'],y=non_snap_foods[metric],
+                           name='Food '+metric+'(Non-SNAP)'))
+    fig.add_trace(go.Scatter(x=snap_foods['date'],y=snap_foods[metric],
+                           name='Food '+metric+'(SNAP)'))
+    fig.update_yaxes(title_text='Total items sold' if metric=='sold' else 'Total revenue($)')
+    fig.update_layout(template='seaborn',title=store)
+    fig.update_layout(
+        xaxis=dict(
+        #autorange=True,
+        range = ['2011-01-29','2016-05-22'],
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1,
+                     label="1m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=6,
+                     label="6m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=1,
+                     label="YTD",
+                     step="year",
+                     stepmode="todate"),
+                dict(count=1,
+                     label="1y",
+                     step="year",
+                     stepmode="backward"),
+                dict(count=2,
+                     label="2y",
+                     step="year",
+                     stepmode="backward"),
+                dict(count=3,
+                     label="3y",
+                     step="year",
+                     stepmode="backward"),
+                dict(count=4,
+                     label="4y",
+                     step="year",
+                     stepmode="backward"),
+                dict(step="all")
+            ])
+        ),
+        rangeslider=dict(
+            autorange=True,
+        ),
+        type="date"
+    ))
+    return fig
+
+
+def calmap(cal_data, state, store, scale):
+    cal_data = cal_data[(cal_data['state_id']==state)&(cal_data['store_id']==store)]
+    years = cal_data.year.unique().tolist()
+    fig = make_subplots(rows=len(years),cols=1,shared_xaxes=True,vertical_spacing=0.005)
+    r=1
+    for year in years:
+        data = cal_data[cal_data['year']==year]
+        data = introduce_nulls(data)
+        fig.add_trace(go.Heatmap(
+            z=data.sold,
+            x=data.week,
+            y=data.day_name,
+            hovertext=data.date.dt.date,
+            coloraxis = "coloraxis",name=year,
+        ),r,1)
+        fig.update_yaxes(title_text=year,tickfont=dict(size=5),row = r,col = 1)
+        r+=1
+    fig.update_xaxes(range=[1,53],tickfont=dict(size=10), nticks=53)
+    fig.update_layout(coloraxis = {'colorscale':scale})
+    fig.update_layout(template='seaborn', title=store)
+    return fig
